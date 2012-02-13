8664f9a2ab2f6c418a5b6b621bd6b8662eed2146
Jan van Haarst <jan.vanhaarst@wur.nl>
2012-02-13 13:10:15 +0100

    Changed output, so we can filter and format it easier.

--- compresstest.sh
+++ compresstest.sh
@@ -22,108 +22,99 @@ export TMPDIR=`pwd`
 # print header
 echo ',,Compression,,,,Decompression,,,,,,,'
 echo 'Type,Setting,Wall clock time,System time,User time,CPU,Wall clock time,System time,User time,CPU,Original size,Uncompressed size,Compressed size,Percentage of original size'
-
-echo gz
 for block in `seq 1 ${END}`
 do
-	echo "$block"
+	echo -e gz"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pigz -${block} -k --suffix .${block}.gz ${NAME}
 	FILE=`mktemp -u tmp.XXXXXXX.gz`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pigz -d -c ${NAME}.${block}.gz > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.gz
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.gz | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.gz
 done
-echo bzip2
 for block in `seq 1 ${END}`
 do
-	echo $block
+	echo -e bzip2"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pbzip2 -${block} -k ${NAME} -c > ${NAME}.${block}.bz2
 	FILE=`mktemp -u tmp.XXXXXXX.bz2`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pbzip2 -d -c ${NAME}.${block}.bz2 > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.bz2
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.bz2 | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.bz2
 done
-echo xz
 for block in `seq 1 ${END}`
 do
-	echo $block
+	echo -e xz"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pixz -t -${block} -i ${NAME} -o ${NAME}.${block}.xz
 	FILE=`mktemp -u tmp.XXXXXXX.xz`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" pixz -t -x -i ${NAME}.${block}.xz -o $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.xz
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.xz | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.xz
 done
-echo 7zip-lzma
 for block in `seq 1 ${END}`
 do
-	echo $block
+	echo -e 7zip-lzma"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" 7zr a -bd -t7z -mmt=on -mx=${block} -m0=lzma ${NAME}.${block}.7z ${NAME}
 	FILE=`mktemp -u tmp.XXXXXXX.7z`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" 7zr e -bd -so -mmt=on ${NAME}.${block}.7z  > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.7z
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.7z | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.7z
 done
-echo 7zip-lzma2
 for block in `seq 1 ${END}`
 do
-	echo $block
+	echo -e 7zip-lzma2"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" 7zr a -bd -t7z -mmt=on -mx=${block} -m0=lzma2 ${NAME}.${block}.7z ${NAME}
 	FILE=`mktemp -u tmp.XXXXXXX.7z2`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" 7zr e -bd -so -mmt=on ${NAME}.${block}.7z  > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.7z
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.7z | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.7z
 done
-echo zip-deflate
 for block in `seq 1 ${END}`
 do
-	echo $block
+	echo -e zip-deflate"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" zip -${block} --quiet ${NAME}.${block}.zip ${NAME}
 	FILE=`mktemp -u tmp.XXXXXXX.zip`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" unzip -p ${NAME}.${block}.zip > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.zip
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.zip | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.zip
 done
 exit;
-echo tamp
 for block in 1
 do
-	echo $block
+	echo -e tamp"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" tamp -i ${NAME} -o ${NAME}.${block}.q
 	FILE=`mktemp -u tmp.XXXXXXX.tamp`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" tamp -d -i ${NAME}.${block}.q -o $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.q
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.q | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.q
 done
-echo lzop
 for block in `seq 1 ${END}`
 do
-	echo "$block"
+	echo -e lzop"\t"$block'%'
 	/usr/bin/time --format "%e\t%S\t%U\t%P" lzop -${block} -k -c ${NAME} > ${NAME}.${block}.lz
 	FILE=`mktemp -u tmp.XXXXXXX.lzop`
 	/usr/bin/time --format "%e\t%S\t%U\t%P" lzop -d -c ${NAME}.${block}.lz > $FILE
-	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.lz
-	echo
+	stat --printf="%s\t" ${NAME} $FILE ${NAME}.${block}.lz | tr -d '\n'
+	echo '%'
 	md5sum $FILE | cut -f 1 -d' '
 	rm $FILE
 	rm ${NAME}.${block}.lz
@@ -135,7 +126,7 @@ done
 # P      Percentage of the CPU that this job got.  This is just user + system times divided by the total running time. It also prints a percentage sign.
 
 # After this is finished do this on the output:
-# sed -i -f filter.sed output.out
-# grep -P -v '^\s*$' < output.out > output.out.cleaned
+# cat galaxy_20120210_150137.csv | grep -f matches.grep  | grep -v Pavlov | sed ':a;N;$!ba;s/%\n/%\t/g' > galaxy_20120210_150137.filtered.csv
+# sed filter from http://stackoverflow.com/questions/1251999/sed-how-can-i-replace-a-newline-n
 
 
