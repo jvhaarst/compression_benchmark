4e248dc35fdabbfc5d00dfca5c4c9ef9d510425e
Jan van Haarst <jan.vanhaarst@wur.nl>
2014-12-20 12:22:20 +0100

    Fixed plotting errors.

--- compressieResults.R
+++ compressieResults.R
@@ -5,6 +5,7 @@ args=(commandArgs(TRUE))
 input=args[1]
 output=args[2]
 type=args[3]
+require(grid)
 library(ggplot2) # if missing, install with : install.packages("ggplot2")
 source("http://egret.psychol.cam.ac.uk/statistics/R/extensions/rnc_ggplot2_border_themes.r") # http://egret.psychol.cam.ac.uk/statistics/R/graphs2.html
 # Read data
@@ -27,20 +28,20 @@ se <- 	ggplot(data, aes(x=Wall.clock.time, y=Percentage.of.original.size,geom = 
 	scale_x_log10() +
 	theme_bw() +
         labs(x = "log duration (seconds)", y = "rate of compression", colour = "Displacement") +
-	opts(
-		title = "Parallel compression benchmark\n blue = compression, red = decompression",
-		panel.grid.major = theme_blank(),
-		panel.grid.minor = theme_blank(),
-		panel.background = theme_blank(),
-		panel.border = theme_border(c("left","bottom")), # RNC hack, see above
+	labs(title = "Parallel compression benchmark\n blue = compression, red = decompression") +
+	theme(
+		panel.grid.major = element_blank(),
+		panel.grid.minor = element_blank(),
+		panel.background = element_blank(),
+#		panel.border = theme_border(c("left","bottom")), # RNC hack, see above
 		legend.position = c(0.9,0.8),
-		legend.title = theme_blank(),
-		legend.text = theme_text(size=12),
+		legend.title = element_blank(),
+		legend.text = element_text(size=12),
 		legend.key.size = unit(1.5, "lines"),
-		legend.key = theme_blank()
+		legend.key = element_blank()
         )+
         scale_shape(solid = FALSE)+        
-	scale_shape_manual(value=c(0:6,8,9))
+	scale_shape_manual(values=c(0:6,8,9))
 	#+
 	#levels(data$Type) <- c("bzip2, "gz, "xz, "7zip-lzma, "7zip-lzma2, "zip-deflate, "tamp, "lzop")
         #scale_colour_brewer(palette="Paired") #  RColorBrewer::display.brewer.all(n=8, exact.n=FALSE)
